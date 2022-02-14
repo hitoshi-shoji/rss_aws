e0146310b0e1db446c9395567e1db0b918b4020b
hitoshi-shoji <hitoshi.shoji@gmail.com>
2022-02-14 22:53:50 +0900

    commit as of 2022/02/14

--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
 #### settings.py
-
+### "rss_url" は、RSS URLを指定
 rss_url="https://aws.amazon.com/about-aws/whats-new/recent/feed/"
+### 「sqlite_dbname」は、データ格納用sqlite3 DB名(ファイル名)を指定
 sqlite_dbname="rssaws.db"
-output_excelfile="/mnt/c/Users/hitshoji/Desktop/aws_rss_new-"
+## 「output_path」は、Excelファイルの出力先ディレクトリー
+##  指定Pathは、事前に作成しておいてください
+##  Excelファイル名は、aws-rss-new_YYYYMMDD.HHMMSS.xlsx 固定
+output_path="/mnt/c/Users/hitshoji/Desktop/"
